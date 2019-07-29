bafee753eec5b42813e98cde3ec4b525ec1ab811
Codebmk <belindamarion0@gmail.com>
2019-07-29 15:56:16 +0300

    Commit Two

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(window).scroll(function(){ 
+  if ($(this).scrollTop() > 100) { 
+      $('#myBtn').fadeIn(); 
+  } else { 
+      $('#myBtn').fadeOut(); 
+  } 
+}); 
+$('#myBtn').click(function(){ 
+  $("html, body").animate({ scrollTop: 0 }, 600); 
+  return false; 
+}); 
